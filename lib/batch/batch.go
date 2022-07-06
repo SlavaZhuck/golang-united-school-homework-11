415747a634ef11b2c676a6d6d57d1d2ff29ea6f9
Viachaslau_Zhuk <Zhukviachaslau@gmail.com>
2022-07-06 22:19:37 +0300

    init variant

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,7 +1,11 @@
 package batch
 
 import (
+	"context"
+	"log"
 	"time"
+
+	"golang.org/x/sync/semaphore"
 )
 
 type user struct {
@@ -14,5 +18,27 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	ctx := context.TODO()
+	sem := semaphore.NewWeighted(pool)
+	var temp = make([]user, n, n)
+
+	for i := 0; int64(i) < n; i++ {
+		// When maxWorkers goroutines are in flight, Acquire blocks until one of the
+		// workers finishes.
+		if err := sem.Acquire(ctx, 1); err != nil {
+			log.Printf("Failed to acquire semaphore: %v", err)
+			break
+		}
+
+		go func(i int64) {
+			defer sem.Release(1)
+			temp[i] = getOne(int64(i))
+		}(int64(i))
+	}
+
+	if err := sem.Acquire(ctx, pool); err != nil {
+		log.Printf("Failed to acquire semaphore: %v", err)
+	}
+
+	return temp
 }
